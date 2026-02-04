c0b6a36bedfa85efcf14b03647c43805ce8f1bb1
Your Name <you@example.com>
2026-02-04 11:48:52 -0500

    Modification pour faire en sorte que GIT arrête de suivre .env

--- API/config/asgi.py
+++ API/config/asgi.py
@@ -10,6 +10,12 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
+import pymysql
+pymysql.install_as_MySQLdb()
+
+import MySQLdb
+MySQLdb.__version__ = "2.2.1"
+MySQLdb.version_info = (2, 2, 1, "final", 0)
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
