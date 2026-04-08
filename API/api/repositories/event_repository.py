a7f0938cd718c29363d1f6084cd46988ce283ce2
Your Name <you@example.com>
2026-04-08 08:37:00 -0400

    Typed

--- API/api/repositories/event_repository.py
+++ API/api/repositories/event_repository.py
@@ -1,5 +1,6 @@
 from api.models import Event
+from datetime import date
 
 class EventRepository:
-    def create(self, name: str, event_date) -> Event:
+    def create(self, name: str, event_date: date) -> Event:
         return Event.objects.create(name=name, event_date=event_date)
