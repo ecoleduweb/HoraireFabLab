60b51701a07f95a818152051144d0c1325f5636f
JordanMan2003 <tchoukouahamanoel@gmail.com>
2026-04-15 11:05:42 -0400

    nouvelles modif

--- Playwright/playwright.config.ts
+++ Playwright/playwright.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 /**
  * Read environment variables from file.
@@ -10,42 +10,39 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './Tests',
+  timeout: 15000,
+  testDir: "./Tests",
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI
-    ? [['github'], ['html', { open: 'never' }]]
-    : [['list'], ['html', { open: 'never' }]],
+    ? [["github"], ["html", { open: "never" }]]
+    : [["list"], ["html", { open: "never" }]],
 
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  timeout: 120000,
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:5002/',
+    baseURL: "http://localhost:5002/",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
 
-    timezoneId : 'America/Montreal',
-    locale: 'fr-CA',
-    
-    
+    timezoneId: "America/Montreal",
+    locale: "fr-CA",
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome']
-     },
-    }
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
+    },
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
@@ -68,8 +65,9 @@ export default defineConfig({
   ],
 
   webServer: {
-    command: 'cd ../FabLab && npm run playwright',
-    url: 'http://localhost:5002/login',
+    command: "cd ../FabLab && npm run playwright",
+    url: "http://localhost:5002/login",
     reuseExistingServer: false,
-  }
+    timeout: 60000,
+  },
 });
