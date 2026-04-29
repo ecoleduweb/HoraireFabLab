f8d5bc9c614075a2f6f0399af3f9f90de54408a6
Your Name <you@example.com>
2026-04-29 10:45:08 -0400

    gloup

--- FabLab/src/ts/server.ts
+++ FabLab/src/ts/server.ts
@@ -1,6 +1,6 @@
 import { env } from "$env/dynamic/public"
-import { InvalidDataError } from "../customErrore/invalidDataError.ts"
-import { NotFoundError } from "../customErrore/NotFoundError.ts"
+import { InvalidDataError } from "../customError/invalidDataError.ts"
+import { NotFoundError } from "../customError/NotFoundError.ts"
 
 export async function GET<T>(url: string, redirectToLoginOn401?: boolean): Promise<T> {
     try {
